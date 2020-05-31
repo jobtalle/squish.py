231448dff8c8c2ed9b4bcbb051005080fb65f641
Job <jobtalle@hotmail.com>
2020-06-01 00:21:32 +0200

    All javascript sources are collected & concatenated

--- squish.py
+++ squish.py
@@ -1,16 +1,23 @@
 import sys
+import re
+import pathlib
 
 from os import path
 
 
 class Document:
+    PATTERN_JS_FILES = '(?<=<script src=\")[^\"]*'
+
     """
     A document to squish
 
     :param source: The source HTML file
     """
     def __init__(self, source):
-        pass
+        self.__root = str(pathlib.Path(source).parent.absolute())
+
+        with open(source, 'r') as file:
+            self.__source = file.read()
 
     """
     Process a document
@@ -18,7 +25,19 @@ class Document:
     :param target: The target output HTML file
     """
     def process(self, target):
-        print(target)
+        self.__collect_js()
+
+    """
+    Concatenate all referenced javascript includes from the source file
+    """
+    def __collect_js(self):
+        combined = ''
+
+        for script in re.findall(self.PATTERN_JS_FILES, self.__source):
+            with open(self.__root + '\\' + script, 'r') as js:
+                combined += js.read()
+
+        return combined
 
 
 if __name__ == "__main__":
