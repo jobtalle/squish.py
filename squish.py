216229157c67ffc4b7f0fded1310dea867f61698
Job <jobtalle@hotmail.com>
2020-06-01 13:03:34 +0200

    External script and style is moved into the HTML file

--- squish.py
+++ squish.py
@@ -6,10 +6,7 @@ from os import path
 
 
 class Document:
-    PATTERN_JS_FILES = '(?<=<script src=\")[^\"]*'
-
-    """
-    A document to squish
+    """ A document to compress
 
     :param source: The source HTML file
     """
@@ -17,27 +14,141 @@ class Document:
         self.__root = str(pathlib.Path(source).parent.absolute())
 
         with open(source, 'r') as file:
-            self.__source = file.read()
+            self.__source = self.__remove_readability(file.read())
 
-    """
-    Process a document
-    
-    :param target: The target output HTML file
-    """
     def process(self, target):
-        self.__collect_js()
+        """ Process a document
 
-    """
-    Concatenate all referenced javascript includes from the source file
-    """
-    def __collect_js(self):
-        combined = ''
+        :param target: The target output HTML file
+        """
 
-        for script in re.findall(self.PATTERN_JS_FILES, self.__source):
-            with open(self.__root + '\\' + script, 'r') as js:
-                combined += js.read()
+        self.__collect((
+            ('script', 'src', 'script'),
+            ('link', 'href', 'style')))
 
-        return combined
+        with open(target, 'w') as file:
+            file.write(self.__source)
+
+    @staticmethod
+    def __make_tag(tag, contents):
+        """ Make a tag with contents
+
+        :param tag: The tag name
+        :param contents: The tag contents
+        :return: A string containing the tag with its contents
+        """
+
+        return '<' + tag + '>' + contents + '</' + tag + '>'
+
+    @staticmethod
+    def __make_regex_tag(tags):
+        """ Make a regex capturing HTML tags
+
+        :param tags: The tags to search for
+        :return: The compiled regular expression
+        """
+
+        tags = '(' + '|'.join(tags) + ')'
+
+        return re.compile('<' + tags + ' ([^>]*)(?:>(?!</' + tags + '>)|(?:></' + tags + '>))')
+
+    @staticmethod
+    def __make_regex_attribute(attribute):
+        """ Make a regex capturing HTML attributes
+
+        :param attribute: The attribute name
+        :return: The compiled regular expression
+        """
+
+        return re.compile(attribute + '="([^"]*)"')
+
+    @staticmethod
+    def __remove_readability(source):
+        """ Remove all nonfunctional readability symbols from a string
+
+        :return: A compressed string
+        """
+
+        return re.sub(' {2}|\t|\n', '', source)
+
+    @staticmethod
+    def __extract_attribute(string, attribute):
+        """ Extract an HTML attribute from a string
+
+        :param string: The tags of an attribute
+        :param attribute: The attribute name
+        :return: The attribute contents as a string
+        """
+
+        matches = re.findall(Document.__make_regex_attribute(attribute), string)
+
+        if matches:
+            return matches[0]
+
+        return None
+
+    def __extract_tags(self, tags, match):
+        """ Extract all occurrences of a given array of tags and replace the tags by the result of a given function
+
+        :param tags: The tag names
+        :param match: A function to execute on each match which must return the new HTML code to replace the match with
+        """
+
+        self.__source = re.sub(self.__make_regex_tag(tags), match, self.__source)
+
+    def __collect(self, tags):
+        """ Include source files referenced by a tag directly
+
+        :param tags: A tuple of tuples describing the tags and attributes to look for
+        """
+
+        combined = {}
+        last_match = {}
+        last_index = {}
+        attribute = {}
+        include_tag = {}
+
+        for tag in tags:
+            combined[tag[0]] = ''
+            last_match[tag[0]] = None
+            last_index[tag[0]] = self.__source.rfind('<' + tag[0])
+            attribute[tag[0]] = tag[1]
+            include_tag[tag[0]] = tag[2]
+
+        def get_match(match):
+            nonlocal combined
+            nonlocal last_match
+            nonlocal last_index
+
+            tag = match[1]
+            source = self.__extract_attribute(match[2], attribute[tag])
+
+            if source is None:
+                return match[0]
+
+            with open(self.__root + '\\' + source, 'r') as js:
+                source_contents = js.read()
+
+            if last_match[tag] is not None:
+                if last_match[tag].span()[1] == match.span()[0]:
+                    combined[tag] += source_contents
+                else:
+                    last_match[tag] = match
+                    script_contents = combined[tag]
+                    combined[tag] = source_contents
+
+                    return self.__make_tag(include_tag[tag], script_contents)
+            else:
+                combined[tag] = source_contents
+
+            last_match[tag] = match
+
+            if match.span()[0] == last_index[tag]:
+                return self.__make_tag(include_tag[tag], combined[tag])
+
+            return ''
+
+        self.__extract_tags([tag[0] for tag in tags], get_match)
 
 
 if __name__ == "__main__":
